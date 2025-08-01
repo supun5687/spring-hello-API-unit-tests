10dfdc7de87d7c0a1075ebbae83d7053b2a996d8
supun5687 <absupuns95@gmail.com>
2025-08-01 23:27:24 +1200

    first-commit

---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.helloworld.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        logger.info("Request received for /hello");
+        return "Hello, World from Java 21!";
+    }
+}
+
