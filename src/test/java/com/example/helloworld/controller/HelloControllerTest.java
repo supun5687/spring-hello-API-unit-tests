d15e83382ce3f6ee24141f480aa22f5ce56d09f7
supun5687 <absupuns95@gmail.com>
2025-08-12 12:10:40 +1200

    added test class for controller

---
+++ src/test/java/com/example/helloworld/controller/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.helloworld.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testSayHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, World from Java 21!"));
+    }
+}
